f4cda38f07109751f3acd33d33e7b56032cb84b6
André Paulo <andre_duivenvoorden@hotmail.com>
2020-10-12 23:52:25 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-container>
+    <h1>Welcome to Academics Management</h1>
+    Please visit our
+    <nuxt-link to="/students" class="btn btn-link">Students</nuxt-link>
+  </b-container>
+</template>
---
+++ pages/students/_id.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-container>
+    <h4>Student Details:</h4>
+    <p>Username: {{ student.id }}</p>
+    <p>Name: {{ student.name }}</p>
+    <p>Email: {{ student.email }}</p>
+    <p>Course: {{ student.courseName }}</p>
+    <h4>Subjects enrolled:</h4>
+    <b-table
+      v-if="subjects.length"
+      striped
+      over
+      :items="subjects"
+      :fields="subjectFields"
+    />
+    <p v-else>
+      No subjects enrolled.
+    </p>
+    <nuxt-link to="/students">
+      Back
+    </nuxt-link>
+  </b-container>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      student: {},
+      subjects: [],
+      subjectFields: ['code', 'name', 'courseCode', 'courseYear', 'scholarYear']
+    }
+  },
+  computed: {
+    id () {
+      return this.$route.params.id
+    }
+  },
+  created () {
+    this.$axios.$get(`/api/students/${this.id}`)
+      .then((student) => { this.student = student || {} })
+      .then(() => this.$axios.$get(`/api/students/${this.id}/subjects`))
+      .then((subjects) => { this.subjects = subjects })
+  }
+}
+</script>
